914b368b2c1d2584963abb71341201d774a2c266
Upneun Marketing <contact@upneun.co.kr>
2026-05-23 13:16:58 +0900

    add google and naver verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,12 @@ export const metadata: Metadata = {
   title: "NONEP",
   description:
     "NONEP is a global creative label — a place where producers gather, voices take shape, and sound becomes story.",
-  // 아래 항목을 추가했습니다
   verification: {
     google: "Df9XQjUrDtzKjIMrq15yZf6dm4N9NQtcoSptLBiW5uc",
+    // 아래 네이버 코드를 추가했습니다
+    other: {
+      "naver-site-verification": "e68ef463557e0626c908eb2c4d74c7f7f334efe1",
+    },
   },
 };
 
